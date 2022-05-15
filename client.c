4930c8bd2c1d61762427148ad2243493ebe49f83
rafa <jerhemoso18@gmail.com>
2022-05-15 17:25:05 -0300

    vo mudar pro wind joga um vava

--- client.c
+++ client.c
@@ -3,10 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
-int main() {
+#define b17 bzero
+int main()
+{
     int op, id_busca;
     char buffer[1024];
+    char field[512];
+    char *campo;
+    char id[4];
 
     int server_socket;
     struct sockaddr_in server_addr;
@@ -16,7 +20,8 @@ int main() {
     int port = 5566;
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_socket < 0) {
+    if (server_socket < 0)
+    {
         perror("Nao foi possivel criar o socket\n");
         exit(EXIT_FAILURE);
     }
@@ -27,59 +32,162 @@ int main() {
     server_addr.sin_port = port;
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    int n = connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(n < 0) {
+    int n = connect(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    if (n < 0)
+    {
         perror("Nao foi possivel conectar ao servidor\n");
         exit(EXIT_FAILURE);
     }
     printf("Servidor conectado\n");
     // send message
-    bzero(buffer, 1024); // zera o buffer
-    strcpy(buffer, "test");
-    printf("Client Message: %s\n", buffer);
-    send(server_socket, buffer, strlen(buffer), 0);
+    // bzero(buffer, 1024); // zera o buffer
+    // strcpy(buffer, "test");
+    // printf("Client Message: %s\n", buffer);
+    // send(server_socket, buffer, strlen(buffer), 0);
 
-    //receive response
-    bzero(buffer, 1024);
-    recv(server_socket, buffer, sizeof(buffer), 0);
-    printf("Server Response: %s\n", buffer);
+    // // receive response
+    // bzero(buffer, 1024);
+    // recv(server_socket, buffer, sizeof(buffer), 0);
+    // printf("Server Response: %s\n", buffer);
 
-    close(server_socket);
-    printf("Server disconnected\n");
+    // close(server_socket);
+    // printf("Server disconnected\n");
 
-    do {
+    char *buscar = "buscar";
+    char *cadastrar = "cadastrar";
+    char server_response[1024];
+
+    do
+    {
         printf("==========================\n");
         printf("1 - Buscar cadastro\n");
         printf("2 - Inserir cadastro\n");
-        printf("3 - Remover cadastro\n");
-        printf("4 - sair\n");
-        printf("==========================");
+        printf("3 - sair\n");
+        printf("==========================\n");
+        printf("Insira a opcao: ");
         scanf("%d", &op);
-        switch(op) {
-            case 1: // Buscar
-            printf("Digite o ID para realizar a busca:\n");
-            scanf("%d", &id_busca);
+        fgetc(stdin);
+        fflush(stdin);
+        switch (op)
+        {
+        case 1: // Buscar
+            if (send(server_socket, buscar, strlen(buscar), 0) < 0)
+            {
+                perror("Falha ao enviar requisicao\n");
+                exit(EXIT_FAILURE);
+            }
+
+            if (recv(server_socket, server_response, 1024, 0) < 0)
+            {
+                printf("falhou\n");
+                break;
+            }
+
+            printf("%s\n", server_response);
+            printf("%d\n", strcmp(server_response, "Entre com o id a ser buscado: "));
+            if (!strcmp(server_response, "Entre com o id a ser buscado: "))
+            {
+                bzero(server_response, 1024);
+                fflush(stdin);
+                fgets(id, 4, stdin);
+                if (send(server_socket, id, strlen(id), 0) < 0)
+                {
+                    perror("Falha ao enviar requisicao\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (recv(server_socket, server_response, 1024, 0) < 0)
+                {
+                    puts("recv falhou\n");
+                    break;
+                }
+
+                bzero(id, 4);
+
+                // puts(server_response);
+                printf("\n");
+                campo = strtok(server_response, "|");
+                printf("Id: %s\n", campo);
+                campo = strtok(NULL, "|");
+                printf("Nome: %s\n", campo);
+                campo = strtok(NULL, "|");
+                printf("Sobrenome: %s\n", campo);
+                campo = NULL;
+                bzero(server_response, 1024);
+            }
+            break;
+
+        case 2:
+            printf("fon1\n");
+            if (send(server_socket, cadastrar, strlen(cadastrar), 0) < 0)
+            {
+                perror("Falha ao enviar requisicao\n");
+                exit(EXIT_FAILURE);
+            }
+            bzero(server_response, 1024);
+            if (recv(server_socket, server_response, 1024, 0) < 0)
+            {
+                printf("falhou\n");
+                break;
+            }
+
+            printf("|%s|\n", server_response);
+            printf("|%d|\n", strcmp(server_response, "Entre com o id a ser cadastrado: "));
+            if (!strcmp(server_response, "Entre com o id a ser cadastrado: "))
+            {
+
+                bzero(server_response, 1024);
+                bzero(buffer, 1024);
+
+                fflush(stdin);
+
+                fgets(field, 4, stdin);
+                printf("%s\n", field);
+                field[strlen(field) - 1] = '\0';
+                strcat(buffer, field);
+                bzero(field, 512);
+
+                printf("Entre com o nome: \n");
+                fflush(stdin);
+                fgets(field, 512, stdin);
+                field[strlen(field) - 1] = '\0';
+                strcat(buffer, "|");
+                strcat(buffer, field);
+                bzero(field, 512);
+
+                printf("Entre com o sobrenome: \n");
+                fflush(stdin);
+                fgets(field, 512, stdin);
+                field[strlen(field) - 1] = '\0';
+                strcat(buffer, "|");
+                strcat(buffer, field);
+                bzero(field, 512);
+
+                if (send(server_socket, buffer, strlen(buffer), 0) < 0)
+                {
+                    perror("Falha ao enviar requisicao\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (recv(server_socket, server_response, 1024, 0) < 0)
+                {
+                    printf("recv falhou\n");
+                    break;
+                }
+                b17(buffer, 1024);
+                printf("%s\n", server_response);
+                b17(server_response, 1024);
+            }
 
             break;
 
-            case 2: // Inserir
-            printf("Digite as informacoes de cadastro:\n");
-            scanf("%s", buffer);
-
-            break;
-
-            case 3: // Remover
-            break;
-
-            case 4:
+        case 3:
             printf("Saindo\n");
             break;
 
-            default:
+        default:
             printf("Opcao Invalida\n");
         }
-    } while(op != 4);
-    
-
-
+    } while (op != 3);
 }
--- server.c
+++ server.c
@@ -1,21 +1,162 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
 
-int main() {
-    int server_socket, client_socket;
+sem_t semaphore_server;
+
+void *connection_handler(void *socket_desc)
+{
+    int sock = *(int *)socket_desc;
+    int read_size;
+    int a;
+    int found = 0;
+    char *insercao_ok = "Novo cadastro registrado\n";
+    char *requsicao_desconhecida = "Comando desconhecido\n";
+    char *solicita_cadastro = "Entre com o id a ser cadastrado: \0";
+    char *solicita_busca = "Entre com o id a ser buscado: \0";
+    char *nenhum_cadastro = "Nenhum cadastro recebido\n";
+    char *message, client_message[1024], save_buffer[1024];
+    char new_register[1024];
+    char *field;
+    char *myfifo = "/tmp/myfifo";
+    FILE *fp;
+    int fd1, fd2;
+    pid_t childpid;
+
+    mkfifo(myfifo, 0666);
+
+    while ((read_size = recv(sock, client_message, 1024, 0)) > 0)
+    {
+
+        sem_wait(&semaphore_server);
+        if (!strcmp(client_message, "cadastrar"))
+        {
+            bzero(client_message, 1024);
+            // memset(client_message, '\0', strlen(client_message));
+            printf("|%s|", solicita_cadastro);
+            write(sock, solicita_cadastro, strlen(solicita_cadastro));
+            if ((read_size = recv(sock, client_message, 1024, 0)) > 0)
+            {
+                printf("fon17\n");
+                if ((childpid = fork()) == -1)
+                {
+                    perror("FORK FAIL\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (childpid > 0)
+                {
+
+                    client_message[strlen(client_message) - 1] = '\0';
+                    fd1 = open(myfifo, O_WRONLY);
+                    write(fd1, client_message, strlen(client_message));
+                    close(fd1);
+                }
+                if (childpid == 0)
+                {
+                    fd2 = open(myfifo, O_RDONLY);
+                    read(fd2, new_register, 1024);
+                    close(fd2);
+                    int size_register = strlen(new_register);
+                    fp = fopen("memoriacompartilhada.txt", "a+");
+                    fwrite(&size_register, sizeof(int), 1, fp);
+                    fwrite(new_register, strlen(new_register), 1, fp);
+                    fclose(fp);
+                    memset(new_register, '\0', strlen(new_register));
+                    write(sock, insercao_ok, strlen(insercao_ok));
+                    exit(0);
+                }
+                memset(client_message, '\0', strlen(client_message));
+            }
+            else
+            {
+                printf("fon13\n");
+            }
+        }
+        else if (!strcmp(client_message, "buscar"))
+        {
+            printf("fon1\n");
+            sem_getvalue(&semaphore_server, &a);
+            memset(client_message, '\0', strlen(client_message));
+            write(sock, solicita_busca, strlen(solicita_busca));
+            if ((read_size = recv(sock, client_message, 1024, 0)) > 0)
+            {
+                int tam;
+                char read_buffer[1024];
+                client_message[strlen(client_message) - 1] = '\0';
+                fp = fopen("memoriacompartilhada.txt", "r");
+                rewind(fp);
+                found = 0;
+                while (fread(&tam, sizeof(int), 1, fp) && !found)
+                {
+                    fread(read_buffer, tam, 1, fp);
+                    strcpy(save_buffer, read_buffer);
+                    field = strtok(save_buffer, "|");
+                    if (!strcmp(client_message, field))
+                    {
+                        // printf("%s\n", read_buffer);
+                        found = 1;
+                        write(sock, read_buffer, strlen(read_buffer));
+                    }
+                    memset(read_buffer, '\0', strlen(read_buffer));
+                    memset(save_buffer, '\0', strlen(save_buffer));
+                }
+                if (!found)
+                {
+                    write(sock, nenhum_cadastro, strlen(nenhum_cadastro));
+                }
+                memset(client_message, '\0', strlen(client_message));
+                fclose(fp);
+            }
+            else
+            {
+                write(sock, nenhum_cadastro, strlen(nenhum_cadastro));
+            }
+        }
+        else
+        {
+            write(sock, requsicao_desconhecida, strlen(requsicao_desconhecida));
+        }
+        sem_post(&semaphore_server);
+    }
+
+    if (read_size == 0)
+    {
+        puts("Cliente desconectado\n");
+        fflush(stdout);
+    }
+    else if (read_size == -1)
+    {
+        perror("recv falhou\n");
+    }
+
+    free(socket_desc);
+
+    return 0;
+}
+
+int main()
+{
+    int server_socket, client_socket, *new_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
-
+    int fd[2];
     char buffer[1024];
     char *ip = "127.0.0.1";
     int port = 5566;
-
+    sem_init(&semaphore_server, 0, 1);
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_socket < 0) {
+    if (server_socket < 0)
+    {
         perror("Nao foi possivel fazer o socket do servidor\n");
         exit(EXIT_FAILURE);
     }
@@ -26,36 +167,46 @@ int main() {
     server_addr.sin_port = port;
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    int n = bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(n < 0) {
+    int n = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (n < 0)
+    {
         perror("Erro de conexao\n");
         exit(EXIT_FAILURE);
     }
     printf("Conexao com a porta: %d estabelecia com sucesso\n", port);
 
     listen(server_socket, 3); // acho que isso são os 3 servidores
+    pipe(fd);
     printf("Listening...\n");
 
-    // usar aquele pipe aqui
-
-    while(1) {
+    while (1)
+    {
         addr_size = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
         printf("Client conectado\n");
+        pthread_t sniffer_thread;
+        new_socket = malloc(1);
+        *new_socket = client_socket;
+        if (pthread_create(&sniffer_thread, NULL, connection_handler, (void *)new_socket) < 0)
+        {
+            perror("Nao foi possivel criar thread\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Conexao estabelecida\n");
 
-        // receive client message
-        bzero(buffer, 1024); //zera o buffer
-        recv(client_socket, buffer, sizeof(buffer), 0);
-        printf("Client message: %s\n", buffer);
+        // // receive client message
+        // bzero(buffer, 1024); // zera o buffer
+        // recv(client_socket, buffer, sizeof(buffer), 0);
+        // printf("Client message: %s\n", buffer);
 
-        // send response
-        bzero(buffer, 1024);
-        strcpy(buffer, "fon");
-        printf("Server Response: %s\n", buffer);
-        send(client_socket, buffer, strlen(buffer), 0);
+        // // send response
+        // bzero(buffer, 1024);
+        // strcpy(buffer, "fon");
+        // printf("Server Response: %s\n", buffer);
+        // send(client_socket, buffer, strlen(buffer), 0);
 
-        close(client_socket);
-        printf("client disconnected\n");
+        // close(client_socket);
+        // printf("client disconnected\n");
     }
 
     return 0;
